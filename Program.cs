d02626f42eafbf887d4fc2d8b3889ec0b188709e
Mari0309 <123794006+Mari0309@users.noreply.github.com>
2023-02-14 12:50:47 +0300

    feat(lw0): checking a

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Введите переменную а: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if ((a % 3 == 0) & (a % 5 == 0)) {
+                Console.WriteLine("fizzbuzz");
+            } else if (a % 3 == 0) {
+                Console.WriteLine("fizz");
+            } else if (a % 5 == 0) {
+                Console.WriteLine("buzz");
+            }
+        }
+    }
+}
